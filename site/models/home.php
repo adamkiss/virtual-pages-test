f0f47b98604047fa56217f3f73106f412db4aa45
Adam Kiss <iam@adamkiss.com>
2022-11-15 00:09:29 +0100

    Force generated uuid

--- site/models/home.php
+++ site/models/home.php
@@ -4,6 +4,7 @@ use Kirby\Cms\Page;
 use Kirby\Cms\Pages;
 use Kirby\Data\Data;
 use Kirby\Toolkit\Str;
+use Kirby\Uuid\Uuid;
 
 class HomePage extends Page {
     public function children()
@@ -18,7 +19,7 @@ class HomePage extends Page {
                 'num'      => $key+1,
                 'template' => 'product',
                 'model'    => 'product',
-                'content'  => $product
+                'content'  => array_merge($product, ['uuid' => Uuid::generate()])
             ];
         }
 
